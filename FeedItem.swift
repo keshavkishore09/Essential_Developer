326d849065c44930407c62667d833c4ef9074129
Keshav Kishore <keshavkishore09@gmail.com>
2022-10-11 00:04:45 +0900

    Create the FeedItem model

---
+++ FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Keshav Kishore on 10/10/22.
+//
+
+import Foundation
+
+
+
+struct FeedItem {
+    let id:  UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
